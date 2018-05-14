9b3e7e4debc72b32a325e002dd79f000285f23c8
Tania Mahajan <tania.mahajan@nagarro.com>
2018-05-14 20:55:15 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from'express';
+import path from 'path';
+
+const app =express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use('/',express.static(path.join(__dirname,'public')));
+
+app.use('/courses',require('./routes/api/courses').route);
+app.use('/teachers',require('./routes/api/teachers').route);
+app.use('/students',require('./routes/api/students').route);
+app.use('/subjects',require('./routes/api/subjects').route);
+
+app.listen(2678,()=>{
+    console.log("Server started at http://localhost:2678")
+});
